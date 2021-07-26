c8f1a9ad208e5d6074eb73913eec5bdee36810eb
Lee Juhyeon <80099290+JH0123@users.noreply.github.com>
2021-07-26 18:54:20 +0900

    Add files via upload

---
+++ timeNow.js
@@ -0,0 +1,15 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        now: '00:00:00'
+    },
+    methods: {
+        time: function (e) {
+            let date = new Date();
+
+            this.now = date.getHours() + ":" +
+                date.getMinutes() + ":" +
+                date.getSeconds();
+        }
+    }
+});
